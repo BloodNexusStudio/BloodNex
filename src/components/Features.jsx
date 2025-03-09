171b3ef14132d91259378afbd5abfa7abb95c2a3
Blood NEXUS <bloodnexusstudio@gmail.com>
2025-03-09 15:42:29 +0530

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -124,18 +124,18 @@ const Features = () => (
       </BentoTilt>
 
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
-  <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
-    <BentoCard
-      src="videos/feature-6.mp4"
-      title={
-        <>
-         Hy<b>p</b>er Realism 3D
-        </>
-      }
-      description=""
-      isComingSoon
-    />
-  </BentoTilt>
+  <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/features-6.mp4"
+            title={
+              <>
+                Hy<b>p</b>er Realism 3D
+              </>
+            }
+            description="Optimized 3D models "
+            isComingSoon
+          />
+        </BentoTilt>
 
         <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
           <BentoCard
