f5c28b54b51a55b996c49d4248972ea756558319
Blood NEXUS <bloodnexusstudio@gmail.com>
2025-03-31 16:16:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const NavBar = () => {
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
-        <nav className="flex size-full items-center justify-between p-4">
+        <nav className="flex size-full font-semibold text-md items-center justify-start p-4">
           {/* Logo and Product button */}
           <div className="flex items-center gap-7">
             <img src="/img/logo.jpg" alt="logo" className="w-10" />
@@ -82,8 +82,8 @@ const NavBar = () => {
           </div>
 
           {/* Navigation Links and Audio Button */}
-          <div className="flex h-full items-center">
-            <div className="hidden md:block">
+          <div className="flex h-full justify-start items-center">
+            <div className="hidden  md:block">
               {navItems.map((item, index) => (
                 <a
                   key={index}
