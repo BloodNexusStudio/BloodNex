532e6fa9881cbf4a87747c355f086c0e13f01035
Blood NEXUS <bloodnexusstudio@gmail.com>
2025-04-02 18:04:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,31 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ImageSequence from "./components/ImageSequence";
 import AestheticCarousel from "./components/AestheticCarousel";
+import SplineViewer from "./components/SplineViewer"; // Added SplineViewer import
 //import Cube from "./components/Cube";
 
 function App() {
+  // Check if "/play" is in the current URL path
+  const showSplineViewer = window.location.pathname.includes("/play");
+
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar  />
-      <Hero/>
-      <About />
-      <Features />
-      <AestheticCarousel />
-      <Story />
-      {/* <Cube /> */}
-      <Contact />
-      <Footer />
-      <ImageSequence/>
+      {showSplineViewer ? (
+        <SplineViewer />
+      ) : (
+        <>
+          <NavBar />
+          <Hero />
+          <About />
+          <Features />
+          <AestheticCarousel />
+          <Story />
+          {/* <Cube /> */}
+          <Contact />
+          <Footer />
+          <ImageSequence />
+        </>
+      )}
     </main>
   );
 }
